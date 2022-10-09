434183524fa6c5c4e2c752800ec20d83b4cf97db
Pawan Gupta <pawan-crestron>
2022-10-10 03:15:16 +0530

    Reading property values, moved property to bootstrap.yml

--- src/main/java/com/wittgroup/kyn/file/KynFileManagementApplication.java
+++ src/main/java/com/wittgroup/kyn/file/KynFileManagementApplication.java
@@ -3,6 +3,7 @@ package com.wittgroup.kyn.file;
 import io.imagekit.sdk.ImageKit;
 import io.imagekit.sdk.config.Configuration;
 import io.imagekit.sdk.utils.Utils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,21 +13,26 @@ import java.io.IOException;
 @SpringBootApplication
 public class KynFileManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KynFileManagementApplication.class, args);
+    @Value("${imageKit.publicKey}")
+    private String publicKey;
 
-	}
+    @Value("${imageKit.privateKey}")
+    private String privateKey;
 
-	@Bean
-	ImageKit getImageKit(){
-		ImageKit imageKit = ImageKit.getInstance();
-		try {
-			Configuration config = Utils.getSystemConfig(KynFileManagementApplication.class);
-			imageKit.setConfig(config);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-		return imageKit;
-	}
+    @Value("${imageKit.urlEndpoint}")
+    private String urlEndPoint;
+
+    public static void main(String[] args) {
+        SpringApplication.run(KynFileManagementApplication.class, args);
+
+    }
+
+    @Bean
+    ImageKit getImageKit() {
+        ImageKit imageKit = ImageKit.getInstance();
+        Configuration config = new Configuration(publicKey, privateKey, urlEndPoint);
+        imageKit.setConfig(config);
+        return imageKit;
+    }
 
 }
